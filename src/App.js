1c344119854ac7dca648e4b7d95532cb5bb448e8
Muhammed Shittu <coolzyte@gmail.com>
2022-06-25 13:38:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+import data from "./data";
+import List from "./List";
+
+export const Wrapper = styled.main`
+  ${tw`min-h-screen flex items-center justify-center`}
+`;
+
+export const Container = styled.section`
+  ${tw`w-[90vh] my-20 mx-0 max-w-[450px] bg-white rounded py-6 px-8 shadow`}
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.4);
+`;
+
+export const Title = styled.h3`
+  ${tw`text-xl md:text-3xl capitalize font-normal mb-8`}
+`;
+
+export const Button = styled.button`
+  ${tw`py-4
+   px-10
+   lg:px-8
+   xl:px-10
+   w-full
+   inline-flex
+   items-center
+   justify-center
+   text-center text-white text-base capitalize
+   bg-pink-400
+   hover:bg-opacity-90
+   font-normal
+   rounded-md`}
+`;
 
 function App() {
+  const [people, setPeople] = useState(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Container>
+        <Title>{people.length} birthdays today</Title>
+        <List people={people} />
+        <Button onClick={() => setPeople([])}>clear all</Button>
+      </Container>
+    </Wrapper>
   );
 }
 
---
+++ src/List.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+export const PersonDetail = styled.article`
+  ${tw`grid grid-template-columns[auto 1fr] gap-3 mb-6 items-center`}
+`;
+export const Image = styled.img`
+  ${tw`w-20 h-20 object-cover rounded-[50%]`}
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1);
+`;
+function List({ people }) {
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <PersonDetail key={id}>
+            <Image src={image} alt={name} />
+            <div>
+              <h4 className="text-sm md:text-lg mb-2">{name}</h4>
+              <p className="mb-0 text-gray-600">{age} years</p>
+            </div>
+          </PersonDetail>
+        );
+      })}
+    </>
+  );
+}
+
+export default List;
